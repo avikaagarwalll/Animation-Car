48ce3fdd5fb201e5229a2c39fbd20a1f8eb26d7f
Avika Agarwal <110806643+avikaagarwalll@users.noreply.github.com>
2024-09-14 15:44:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3'); 
+audio.loop=true;
+audio.play();
+
+// for the audio go to safari top left 
+// then go to settings and then go to websites and then go autoplay then select allow all autoplay.
